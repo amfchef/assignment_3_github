a6c14c144cd4d6dc33f0364a729ba12b2d66e053
Jakob Johansson <jakobtj@hotmail.com>
2020-10-02 17:12:38 +0200

    modified primenum method

--- src/Johansson/Main.java
+++ src/Johansson/Main.java
@@ -3,6 +3,29 @@ package Johansson;
 import java.util.Scanner;
 
 public class Main {
+    public static void primeNum(int checkPrimeNumb){
+        String primeNumbers = "";
+        if (checkPrimeNumb <= 1) {
+            System.out.println("The maximum value you entered is too low");
+            System.exit(0);
+
+        } else {
+            for (int i = 2; i <= checkPrimeNumb; i++) {
+                int counter = 0;
+                for (int j = i; j >= 1; j--) {
+                    if (i % j == 0) {
+                        counter = counter + 1;
+                    }
+                }
+                if (counter == 2) {
+
+                    primeNumbers = primeNumbers + i + " ";
+                }
+            }
+            System.out
+                    .println("Prime numbers with the maximum of " + checkPrimeNumb + " is the below:\n" + primeNumbers);
+        }
+    }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
