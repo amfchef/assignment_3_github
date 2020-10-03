f9be5d092221cb4704090282e9c675544c997f2a
Jakob Johansson <jakobtj@hotmail.com>
2020-10-03 10:53:06 +0200

    modified the Prime Num method

--- src/Johansson/Main.java
+++ src/Johansson/Main.java
@@ -3,11 +3,10 @@ package Johansson;
 import java.util.Scanner;
 
 public class Main {
-    public static void primeNum(int checkPrimeNumb){
+    public static void primeNum(int checkPrimeNumb){    //this method will use the user unput and print out all prime numbers below the input
         String primeNumbers = "";
-        if (checkPrimeNumb <= 1) {
+        if (checkPrimeNumb <= 1) {  //there is no primeNums below 2
             System.out.println("The maximum value you entered is too low");
-            System.exit(0);
 
         } else {
             for (int i = 2; i <= checkPrimeNumb; i++) {
@@ -18,48 +17,24 @@ public class Main {
                     }
                 }
                 if (counter == 2) {
-
                     primeNumbers = primeNumbers + i + " ";
                 }
             }
-
                     System.out.println("Prime numbers with the maximum of " + checkPrimeNumb + " is the below:\n" + primeNumbers);
         }
     }
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int checkPrimeNumb = 0;
         System.out.print("Please enter a maximum number of primenumber you would like to print out: ");
-
         try {
             checkPrimeNumb = scan.nextInt();
         } catch (Exception e) {
             System.out.println("Error\nInvalid format\nYou have to enter a number");
             System.exit(0);
-
         }
         scan.close();
-        String primeNumbers = "";
-        if (checkPrimeNumb <= 1) {
-            System.out.println("The maximum value you entered is too low");
-            System.exit(0);
-
-        } else {
-            for (int i = 2; i <= checkPrimeNumb; i++) {
-                int counter = 0;
-                for (int j = i; j >= 1; j--) {
-                    if (i % j == 0) {
-                        counter = counter + 1;
-                    }
-                }
-                if (counter == 2) {
-
-                    primeNumbers = primeNumbers + i + " ";
-                }
-            }
-
-        }
+           primeNum(checkPrimeNumb);
         System.exit(0);
     }
 }
